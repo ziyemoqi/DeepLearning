7b9e98ce828f95a7e1103134b698e8b7bee01a8e
Kroos-x <1197798263@qq.com>
2021-11-25 14:22:31 +0800

    Update Continue from test1

---
+++ dl-practice/src/main/java/com/yc/practice/demo/controller/Test2Controller.java
@@ -0,0 +1,29 @@
+package com.yc.practice.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 功能描述:导入导出 控制层
+ *
+ * @Author: xieyc
+ * @Date: 2020-02-16
+ * @Version: 1.0.0
+ */
+@RestController
+@RequestMapping("/demo/test")
+@RequiredArgsConstructor
+@Validated
+public class Test2Controller {
+
+    @GetMapping
+    public void test(@NotBlank(message = "用户名不能为空") String param) {
+        System.out.println(param);
+    }
+
+}
