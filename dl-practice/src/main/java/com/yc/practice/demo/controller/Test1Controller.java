c876218b7c0bfca8088b6031516f58160185227c
Kroos-x <1197798263@qq.com>
2021-11-25 14:21:51 +0800

    Update Continue

--- dl-practice/src/main/java/com/yc/practice/demo/controller/Test1Controller.java
+++ dl-practice/src/main/java/com/yc/practice/demo/controller/Test1Controller.java
@@ -1,8 +1,6 @@
 package com.yc.practice.demo.controller;
 
-import com.yc.core.test.TestParam;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +19,7 @@ import javax.validation.constraints.NotBlank;
 @RequestMapping("/demo/test")
 @RequiredArgsConstructor
 @Validated
-public class TestController {
+public class Test1Controller {
 
     @GetMapping
     public void test(@NotBlank(message = "用户名不能为空") String param) {
