877ec3ca90196096923b1cab49e7752b8475038b
Kroos-x <1197798263@qq.com>
2021-11-25 14:03:02 +0800

    Update Continue

--- dl-practice/src/main/java/com/yc/practice/demo/controller/TestController.java
+++ dl-practice/src/main/java/com/yc/practice/demo/controller/TestController.java
@@ -27,6 +27,7 @@ public class TestController {
     public void test(@NotBlank(message = "用户名不能为空") String param) {
         System.out.println(param);
         System.out.println(param);
+        System.out.println(1);
     }
 
 }
